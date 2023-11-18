1d3b3d40f9d7b03325becb116bdd6bdf24a9aca1
rhose1234 <akorederhose@gmail.com>
2023-11-18 23:09:45 +0100

    jnj

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-red-400'>rose welcome</h1>
+        </div>
+    );
+};
+
+export default Home;
